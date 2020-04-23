6aa88971632e5599663b6fb7609d6b9dae72b9a8
broliang <18706703437@163.com>
2020-04-23 21:15:10 +0800

    Add 01_higher_order_procedures/{function_as_data,test}.el: Add more test.

--- 01_higher_order_procedures/function_as_data.el
+++ 01_higher_order_procedures/function_as_data.el
@@ -1,3 +1,7 @@
+;; Higher order procedures generalize a pattern
+;; Generalizing patterns is how we keep our program from getting really long
+;; Programming is easy as long as we can see the whole program all at once
+
 (defun my-sum (fn-as-data a b)
   (if (> a b)
       0
@@ -6,5 +10,17 @@
   )
 
 (defun square (x) (* x x))
+
+(defun cube (x) (* x x x))
+
+
+(defun my-choose (fn-as-data my-list)
+  (cond ((null my-list) '())
+        ((funcall fn-as-data (first my-list))
+         (cons (first my-list) (my-choose fn-as-data (cdr my-list))))
+        (t (my-choose fn-as-data (cdr my-list)))
+        )
+  )
+
 
 (provide 'function_as_data)
--- 01_higher_order_procedures/test.el
+++ 01_higher_order_procedures/test.el
@@ -1,7 +1,21 @@
 (require 'ert)
+(require 'cl-lib)
+
 (require 'function_as_data)
 
-(ert-deftest function-as-data-test ()
+(ert-deftest my-sum-test ()
   ;; 3*3 + 4*4 + 5*5 = 50
   (should (equal (my-sum 'square 3 5) 50))
+  ;; 3*3*3 + 4*4*4 + 5*5*5 = 216
+  (should (equal (my-sum 'cube 3 5) 216))
+  )
+
+(ert-deftest my-choose-test ()
+  ;; Choose even number from list
+  (should (equal (my-choose 'evenp '(1 3 4 5 8 9))
+                 '(4 8)))
+  ;; Choose string that contains letter e
+  (should (equal (my-choose (lambda (str) (s-contains-p "e" str))
+                            '("gotta" "get" "you" "in" "my" "life"))
+                 '("get" "life")))
   )
