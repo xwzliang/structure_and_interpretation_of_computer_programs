c11c34367f8cbef526f26d7d5189d479ebafb431
broliang <18706703437@163.com>
2020-05-08 13:27:08 +0800

    New 03_object_oriented/persistent_local_state_variables.el and Add test for it.

---
+++ 03_object_oriented/build_class_using_lambda.el
@@ -0,0 +1,102 @@
+;; Dynamic and Lexical Binding
+
+;; dynamic
+;; All variable names and their values live in one global table. A name that is dynamically bound is looked up only in bindings in the dynamic environment of the name – that is, in all bindings which have been created since the program began and which have not yet been destroyed. When there are multiple bindings in the dynamic environment, the most recently created one is used.
+;; lexical
+;; Each binding scope (function, let syntax, …) creates a new table of variable names and values, organised in a hierarchy called “the environment”. A name that is lexically bound is looked up only in bindings in the lexical environment of the name – that is, in bindings that enclose the name in the source code.
+
+;; If lexical binding is needed for whole file without using lexical-let, put following line to the first line of a file
+;;; -*- lexical-binding: t -*-
+
+(defalias 'one-count
+  (lexical-let ((count-value 0))
+    (lambda ()
+      (setq count-value (+ count-value 1))
+      count-value)
+    )
+  )
+;; (let ((x 0)) exp-body) is just a syntactic sugar for ((lambda (x) exp-body) 0)
+;; a lambda function and then call it with argument
+
+;; if count-value is not lexically bound, the counter won't work
+;; because when dynamically bount, the variable count-value has been destroyed by the time lambda is applied
+
+;; A factory to make many counters
+(defun make-count ()
+  (lexical-let ((count-value 0))
+    (lambda ()
+      (setq count-value (+ count-value 1))
+      count-value)
+    )
+  )
+
+
+;; Actually we are making a class object here which has global class variables and local instance variables
+;; using only primitives
+(defalias 'make-count-global-and-local
+  (lexical-let ((global-value 0))
+    (lambda ()
+      (lexical-let ((local-value 0))
+        (lambda ()
+          (setq local-value (+ local-value 1))
+          (setq global-value (+ global-value 1))
+          (list local-value global-value)
+          )
+        )
+      )
+    )
+  )
+
+
+;; A class object with message passing
+(defalias 'make-count-global-and-local-with-message-passing
+  (lexical-let ((global-value 0))
+    (lambda ()
+      (lexical-let ((local-value 0))
+        (lambda (message)   ;; dispatch procedure that takes a message, and decide which procedure to return
+          ;; An object is represented by its dispatch procedure
+          (cond ((equal message 'local)
+                 (lambda ()
+                   (setq local-value (+ local-value 1))
+                   local-value))
+                ((equal message 'global)
+                 (lambda ()
+                   (setq global-value (+ global-value 1))
+                   global-value))
+                ((equal message 'set-local-value)
+                 (lambda (new-value)
+                   (setq local-value new-value)
+                   (message "okay")))
+                (t (error "No such method" message))
+                )
+          )
+        )
+      )
+    )
+  )
+
+
+;; Class with inheritance
+;; a buzzer returns buzz if the number is dividable by 7
+(defalias 'make-buzzer
+  (lambda ()
+    (lexical-let ((parent-count (make-count-global-and-local-with-message-passing)))
+      (lambda (message)
+        (cond ((equal message 'local)
+               (lambda ()
+                 (let ((count-value (funcall (funcall parent-count 'local))))
+                   (if (equal (% count-value 7) 0)
+                       (message "buzz")
+                       count-value)
+                   )
+                 )
+               )
+              ;; other message just passed to parent to handle
+              (t (funcall parent-count message))
+              )
+        )
+      )
+    )
+  )
+
+(provide 'build_class_using_lambda)
---
+++ 03_object_oriented/test.el
@@ -0,0 +1,61 @@
+(require 'build_class_using_lambda)
+
+(ert-deftest one-count-test ()
+  ;; These three should test only once without re-evaluate the source code
+  ;; because every call (one-count) will add 1
+  (should (equal (one-count) 1))
+  (should (equal (one-count) 2))
+  (should (equal (one-count) 3))
+  )
+
+(ert-deftest make-count-test ()
+  (defalias 'count1 (make-count))
+  (defalias 'count2 (make-count))
+
+  (should (equal (count1) 1))
+  (should (equal (count1) 2))
+  (should (equal (count1) 3))
+  (should (equal (count2) 1))
+  (should (equal (count2) 2))
+  (should (equal (count1) 4))
+  (should (equal (count1) 5))
+  )
+
+(ert-deftest global-and-local-count-test ()
+  (defalias 'global-and-local-count1 (make-count-global-and-local))
+  (defalias 'global-and-local-count2 (make-count-global-and-local))
+
+  ;; Each counter has its own local count value and also the global count value
+  (should (equal (global-and-local-count1) '(1 1)))
+  (should (equal (global-and-local-count1) '(2 2)))
+  (should (equal (global-and-local-count2) '(1 3)))
+  (should (equal (global-and-local-count1) '(3 4)))
+  (should (equal (global-and-local-count2) '(2 5)))
+  )
+
+(ert-deftest global-and-local-with-message-passing-count-test ()
+  (defalias 'global-and-local-with-message-passing-count1 (make-count-global-and-local-with-message-passing))
+  (defalias 'global-and-local-with-message-passing-count2 (make-count-global-and-local-with-message-passing))
+
+  (should (equal (funcall (global-and-local-with-message-passing-count1 'local)) 1))
+  (should (equal (funcall (global-and-local-with-message-passing-count1 'local)) 2))
+  (should (equal (funcall (global-and-local-with-message-passing-count1 'global)) 1))
+  (should (equal (funcall (global-and-local-with-message-passing-count1 'global)) 2))
+  (should (equal (funcall (global-and-local-with-message-passing-count2 'local)) 1))
+  (should (equal (funcall (global-and-local-with-message-passing-count2 'global)) 3))
+
+  ;; set local value of count2 to 8
+  (should (equal (funcall (global-and-local-with-message-passing-count2 'set-local-value) 8) "okay"))
+  (should (equal (funcall (global-and-local-with-message-passing-count2 'local)) 9))
+  )
+
+(ert-deftest make-buzzer-test ()
+  (defalias 'buzzer (make-buzzer))
+  (should (equal (funcall (buzzer 'local)) 1))
+  (should (equal (funcall (buzzer 'local)) 2))
+  (should (equal (funcall (buzzer 'set-local-value) 5) "okay"))
+  (should (equal (funcall (buzzer 'local)) 6))
+  (should (equal (funcall (buzzer 'local)) "buzz"))
+  (should (equal (funcall (buzzer 'set-local-value) 13) "okay"))
+  (should (equal (funcall (buzzer 'local)) "buzz"))
+  )
